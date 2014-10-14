7a573ac7a820be780c181d5fd78aed2704c98561
Josh McDonald <jm.sentry@gmail.com>
2014-10-14 18:15:44 -0500

    Update user function added.
    Also took first steps to implement secure password storage.

--- index.php
+++ index.php
@@ -1,43 +1,76 @@
 <?php
 	require "./include/config.inc.php";
+	//$userObj = new User();
 	
-	///////////////////
-	//Update User Info
-	//$user->id = "4";	
-	//$user->Age = "32";
-	//$saved = $user->Save();
-	//
-	//Delete User
-	//$user->id = "17";	
-	//$delete = $user->Delete();
-	//////////////////
+	/*
+	*	Todo list
+	*-------------------
+	*Update User Info
+	*$user->id = "4";	
+	*$user->Age = "32";
+	*$saved = $user->Save();
+	*
+	*Delete User
+	*$user->id = "17";	
+	*$delete = $user->Delete();
+	*/
 
-	$create = newUser(new User(),"test@gmail.com","test1","test1");
-	d($create, "newUser Returned:");
-	
-	
-	function d($var,$txt){
-		echo '<pre>';
-		echo '<h1>' . $txt. '</h1>';
-		var_dump($var);
-		echo '</pre>';
+	/*
+	*	Create new user
+	*/
+	try {
+		$create = newUser(new User(), $email, $username, $password, $salt);
+		if ($debug) d($create, "newUser Returned: ");
+	} catch (Exception $e) {
+		if ($debug) echo 'Caught exception: ',  $e->getMessage(), "\n";
 	}
 	
-	function newUser($user, $email, $username, $password){
-		$users = $user->all();
-		foreach($users as $each){
+	/*
+	*	Update user field
+	*/
+	try {
+		$update = updateUser(new User(), "1", "field", "value");
+		if ($debug) d($update, "updateUser Returned: ");
+	} catch (Exception $e) {
+		if ($debug) echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+	/*------------------------------------------------------
+	*					BEGIN FUNCTIONS
+	*------------------------------------------------------*/
+	function d($var,$txt){
+		echo '<pre><code>';
+		echo '<b>' . $txt. '</b>';
+		var_dump($var);
+		echo '</code></pre>';
+	}
+	
+	function newUser($userObj, $email, $username, $password, $salt){
+		$allUsers = $userObj->all();
+		foreach($allUsers as $each){
 			if (array_search($email, array_values($each))){
 				return false;
 			}
 		}
-		$user->email = $email;
-		$user->username  = $username;
-		$user->password = $password;
+		
+		$userObj->email = $email;
+		$userObj->username  = $username;
+		$userObj->password = hash_hmac('sha256', $password, $salt);
 
-		if ($user->Create()){
-			return $user;
+		if ($userObj->create()){
+			return true;
 		} else {
-			return -1;
+			throw new Exception('Create function has failed!');
+		}
+	}
+	
+	function updateUser($userObj, $userID, $field, $value){
+		$userObj->id = $userID;	
+		$userObj->$field = $value;
+
+		if ($userObj->save()){
+			return true;
+		} else {
+			throw new Exception('Update function has failed!');
 		}
 	}
 ?>
